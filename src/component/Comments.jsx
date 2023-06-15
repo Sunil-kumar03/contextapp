0acb344433a2bc7c852e461bed5a9cb61ef04660
Sunil-kumar03 <challasunil2002@gmail.com>
2023-06-15 22:49:36 +0530

    use context completed

--- src/component/Comments.jsx
+++ src/component/Comments.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { DataContext } from '../Context'
 
 function Comments() {
+    const context = useContext(DataContext)
+    const [comments]=context.commentApi.comments
+
   return (
-    <div className='container'>
-      <h1>comments</h1>
+    <div className="container">
+      <div className="row">
+          <div className="col-md-12 text-center">
+            <div className="display-3 text-success">Comments</div>
+          </div>
+      </div>
+      <div className="row">
+        {
+          comments && comments.map((item,index)=>{
+            return(
+              <div className="col-md-3 mt-2 mb-2">
+                <div className="card">
+                  <div className="card-header"><h4>{item.id}{item.name}</h4></div>
+                  <div className="card-body"><p className="text-primary">{item.body}</p></div>
+                  <div className="card-footer"><p className="text-success float-end">{item.email}</p></div>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
--- src/component/Posts.jsx
+++ src/component/Posts.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { DataContext } from '../Context'
 
 function Posts() {
+  const context = useContext(DataContext)
+  const [posts]= context.postApi.posts
   return (
     <div className='container'>
-      <h1>Posts</h1>
+      <div className="row">
+        <div className="col-md-12 text-center">
+          <div className="display-3 text-success">
+            Posts
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        {
+          posts && posts.map((item,index) =>{
+            return(
+              <div className="col-md-4 mt-2 mb-2" key={index}>
+              <div className="card">
+                <div className="card-header">
+                  <h6 className="text-center">{item.id}{item.title}</h6>
+                </div>
+                <div className="card-body">
+                  <p className="text-secondary">{item.body}</p>
+                </div>
+              </div>
+            </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
